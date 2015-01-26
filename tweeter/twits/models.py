34569327dcf67ac7dc4543116fd2fcbd83b651c1
Yiwei Gao <ygao56@emory.edu>
2015-01-26 17:03:00 -0500

    Changed directory structure

--- tweeter/twits/models.py
+++ tweeter/twits/models.py
@@ -1,14 +1,21 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Twit(models.Model):
 	text = models.CharField(max_length=140)
-	date = models.DateTimeField('date twitted')
+	user = models.ForeignKey(User)
+	created_date = models.DateTimeField(auto_now=True, blank=False)
 
 	def __unicode__(self):
 		return self.text
 
 	def was_published_recently(self):
-		return self.date >= timezone.now() - datetime.timedelta(days=1)
+		return self.created_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Twitter(models.Model):
+	name = models.OneToOneField(User)
+	following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
