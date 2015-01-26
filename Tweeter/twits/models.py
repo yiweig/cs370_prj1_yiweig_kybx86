02a2c73ed12be815d678ac94c675fda420ce4f31
Yiwei Gao <ygao56@emory.edu>
2015-01-26 12:13:14 -0500

    "Deleted old directory because I was looking at a tutorial for Django 1.7....needed 1.6"

--- Tweeter/twits/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Twit(models.Model):
-	twit_text = models.CharField(max_length=140)
-	twit_date = models.DateTimeField('date twitted')
